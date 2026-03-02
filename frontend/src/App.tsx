d7e7d5a9bb597e07e9d59d17e2e467703c2974d3
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2026-03-02 18:59:13 +0800

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MoviesPage from "@/pages/movies/MoviesPage";
 import MovieDetailPage from "@/pages/movies/MovieDetailPage";
 import { AddMovie } from "./modules/movies/components/AddMovie";
+import { AuthProvderEffect } from "./modules/admin/auth/AuthProviderEffect";
+import { AdminLayout } from "./modules/admin/AdminLayout";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +22,12 @@ const App = () => (
         <Route path="/" element={<MoviesPage />} />
         <Route path="/addMovie" element={<AddMovie />} />
         <Route path="/movies/:id" element={<MovieDetailPage />} />
+        <Route element={<AuthProvderEffect />}>
+          <Route path="admin" element={<AdminLayout />}>
+            <Route path="create-movie" element={<div>create-movie</div>} />
+            <Route path="get-comments" element={<div>get-movies</div>} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </QueryClientProvider>
