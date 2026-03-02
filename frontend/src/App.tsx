33b90ad22dea3f0ab933a81537ebd3fa7aeab6f5
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2026-03-02 20:38:28 +0800

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MoviesPage from "@/pages/movies/MoviesPage";
 import MovieDetailPage from "@/pages/movies/MovieDetailPage";
-import { AddMovie } from "./modules/movies/components/AddMovie";
-import { AuthProvderEffect } from "./modules/admin/auth/AuthProviderEffect";
 import { AdminLayout } from "./modules/admin/AdminLayout";
+import { AuthProviderEffect } from "./modules/admin/auth/AuthProviderEffect";
+import { Login } from "./modules/admin/auth/login/Login";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,10 +20,11 @@ const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MoviesPage />} />
-        <Route path="/addMovie" element={<AddMovie />} />
         <Route path="/movies/:id" element={<MovieDetailPage />} />
-        <Route element={<AuthProvderEffect />}>
-          <Route path="admin" element={<AdminLayout />}>
+
+        <Route path="admin" element={<AdminLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route element={<AuthProviderEffect />}>
             <Route path="create-movie" element={<div>create-movie</div>} />
             <Route path="get-comments" element={<div>get-movies</div>} />
           </Route>
